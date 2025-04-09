79e3c37c7fafd4bd2b63e12a799027d398f0fda8
Aditi Das <aditi000das@gmail.com>
2025-04-09 23:59:38 +0530

    tutor js done

--- adaptive AI/script.js
+++ adaptive AI/script.js
@@ -35,4 +35,50 @@ async function generateResponse(){
           }]
        })
        }
+    };
+    try{
+        loading.style.display="block";
+        let response=await fetch(API_URL,requestOptions);
+        let data=await response.json();
+
+        if(data.candidates && data.candidates.length>0){
+            let apiResponse=data.candidates[0].content.parts[0].text.replace(/\*\*(.?)\\*/g,"$1").trim();
+            text.innerHTML = apiResponse;
+            output.style.display = "block";
+        } else {
+            console.error("No valid response from API.");
+        }
+    } catch (e) {
+        console.error("Error fetching response:", e);
+    } finally {
+        loading.style.display = "none";
     }
+
+
+input.addEventListener("change", () => {
+    const file = input.files[0];
+    if (!file) return;
+
+    let reader = new FileReader();
+    reader.onload = (e) => {
+        let base64data = e.target.result.split(",")[1];
+        fileDetails.mime_type = file.type;
+        fileDetails.data = base64data;
+
+        innerUploadImage.querySelector("span").style.display = "none";
+        innerUploadImage.querySelector("#icon").style.display = "none";
+        image.style.display = "block";
+        image.src = data.$;{fileDetails.mime_type};base64,$
+        {fileDetails.data};
+        output.style.display = "none";
+    };
+    reader.readAsDataURL(file);
+});
+
+btn.addEventListener("click", () => {
+    generateResponse();
+});
+
+innerUploadImage.addEventListener("click", () => {
+    input.click();
+});
