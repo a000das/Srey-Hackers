a0da9292a860ed8b79a4d716f864c2280ec938a0
Aditi Das <aditi000das@gmail.com>
2025-04-09 11:57:43 +0530

    merge

--- script.js
+++ script.js
@@ -14,3 +14,40 @@ let user={
         data: null
     }
 }
+async function generateResponce(aiChatBox) {
+    let text=aiChatBox.querySelector(".ai-chat-area")
+      let RequestOption={
+        method="POST",
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({
+            "contents":[
+                {"parts":[{"text":user.message},(user.file.data?[{"inline_data":user.file}]:[])
+            ]
+        }
+            ]
+        })
+
+      }
+    try{
+        let responce= await fetch(Api_Url,RequestOption)
+        let data=await responce.json()
+        let apiResponce=data.candidates[0].content.parts[0].text.replace(/\*\*(.?)\\*/g,"$1").trim()
+        text.innerHTML=apiResponce
+    }
+
+    catch(error){
+        console.log(error);
+
+    }
+    
+    finally{
+        chatContainer.scrollTo({top:chatContainer.scrollHeight,behavior:"smooth"})
+        image.src= img.svg
+     image.classList.remove("choose")
+     user.file={}
+    }
+    
+
+}
+
+
